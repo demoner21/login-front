2358bc73899d5b11cb29f838bd81ba5fa7c7c513
demoner21 <a.tranhago@gmail.com>
2025-11-20 22:56:55 -0300

    feature: add loading stats

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,15 +3,20 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = () => {
-    const { isAuthenticated } = useAuth(); // Pega o estado do nosso contexto
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="text-lg">Carregando...</div>
+            </div>
+        );
+    }
 
     if (!isAuthenticated) {
-        // 1. Se não estiver logado, redireciona para /login
-        // 'replace' impede o usuário de voltar para a página protegida no histórico
         return <Navigate to="/login" replace />;
     }
 
-    // 2. Se estiver logado, renderiza o conteúdo da rota (ex: DashboardPage)
     return <Outlet />;
 };
 
